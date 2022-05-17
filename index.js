0dae71e6edf0821f9087d908742eda03ba13ba32
Brian Allen <vecter1180@gmail.com>
2022-05-17 01:51:44 -0400

    commit tree a0c86208f786e3686f6e0bffa26cba8dda40cee2

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 
-// Write your code here
+let num1 = 2;
+let num2 = 31;
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random() * 10 + 1)
+
+let num3 = 100
+let num4 = 25
+let mod = num3/num4
+
+let max = Math.max(1,4,8,15,20)
+
+
+
+
+
+
+
